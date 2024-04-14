85d55502b75ecd5465e24862c6f994c8198b84e0
Harsh Dagar <amandagar000@gmail.com>
2024-04-14 23:59:37 +0530

    added types for app/web

---
+++ apps/web/@types/index.d.ts
@@ -0,0 +1,11 @@
+export type RestProps = {
+  [key: string]: any;
+};
+export type NextPageProps = {
+  params: {
+    [key: string]: string;
+  };
+  searchParams: {
+    [key: string]: string;
+  };
+};
