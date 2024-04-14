062b1a5237e7fdbb32db564b314d8ca3c1a7362f
Harsh Dagar <amandagar000@gmail.com>
2024-04-15 00:21:34 +0530

    added category listing

---
+++ apps/web/app/listing/[category]/[page]/page.tsx
@@ -0,0 +1,102 @@
+import { Product } from "@repo/ui/types";
+import { ProductTemplate } from "../../../../components";
+import { NextPageProps } from "../../../../@types";
+import { Suspense } from "react";
+import { Pagination as PgNation } from "@repo/ui";
+
+const {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} = PgNation;
+
+export default async function ListingPage({
+  params: { category, page },
+  searchParams,
+}: NextPageProps) {
+  //TODO: Implement listing based on URL params and path
+  const url = new URL(
+    `${process.env.productAPI}/products${category !== "all" ? `/category/${category}` : ""}`
+  );
+  const currentPage = Number(page);
+  const size = Number(searchParams.size) || 20;
+  url.searchParams.append("limit", String(size));
+  url.searchParams.append("skip", String(size * (currentPage - 1)));
+
+  //   for (const key in searchParams) {
+  //     const value = searchParams[key];
+  //     url.searchParams.append(key, value);
+  //   }
+  const data = await fetch(url.toString()).then((res) => res.json());
+  const dataSize = data.limit;
+  const dataTotal = data.total;
+  const dataSkipped = data.skip;
+
+  return (
+    <div className="col-span-3 p-4">
+      <div className="flex flex-wrap gap-x-4 gap-y-8 max-md:gap-x-2 max-md:gap-y-4">
+        <Suspense fallback={<div>Loading...</div>}>
+          {data.products?.map((product: Product) => (
+            <ProductTemplate
+              product={product}
+              className="md:w-60 md:h-72 max-md:w-40 max-md:h-40"
+              key={product.id}
+            />
+          ))}
+        </Suspense>
+      </div>
+      <div className="text-center text-gray-400 my-4 fieldset-legend">
+        Showing {dataSize} results
+      </div>
+      {dataSize + dataSkipped <= dataTotal && (
+        <Pagination>
+          <PaginationContent>
+            {currentPage > 1 && (
+              <>
+                <PaginationItem>
+                  <PaginationPrevious href={String(currentPage - 1)} />
+                </PaginationItem>
+                {currentPage > 2 && (
+                  <PaginationItem>
+                    <PaginationEllipsis />
+                  </PaginationItem>
+                )}
+                <PaginationItem>
+                  <PaginationLink href={String(currentPage - 1)}>
+                    {currentPage - 1}
+                  </PaginationLink>
+                </PaginationItem>
+              </>
+            )}
+            <PaginationItem>
+              <PaginationLink href={String(currentPage)}>
+                {currentPage}
+              </PaginationLink>
+            </PaginationItem>
+            {dataSize * currentPage < dataTotal && (
+              <>
+                <PaginationItem>
+                  <PaginationLink href={String(currentPage + 1)}>
+                    {currentPage + 1}
+                  </PaginationLink>
+                </PaginationItem>
+                {dataTotal - dataSkipped - 2 * dataSize > 0 && (
+                  <PaginationItem>
+                    <PaginationEllipsis />
+                  </PaginationItem>
+                )}
+                <PaginationItem>
+                  <PaginationNext href={String(currentPage + 1)} />
+                </PaginationItem>
+              </>
+            )}
+          </PaginationContent>
+        </Pagination>
+      )}
+    </div>
+  );
+}
---
+++ apps/web/app/listing/template.tsx
@@ -0,0 +1,15 @@
+type ListingProp = {
+  children: React.ReactNode;
+  params: {
+    [key: string]: string;
+  };
+};
+
+export default function ListingTemplate({ children, ...rest }: ListingProp) {
+  return (
+    <div className="grid grid-cols-4 px-common-x gap-10">
+      <div className="border max-64">Filters</div>
+      {children}
+    </div>
+  );
+}
