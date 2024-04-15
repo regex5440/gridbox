8cf00c202624cd754289199594121acff52e7ccd
Harsh Dagar <amandagar000@gmail.com>
2024-04-16 02:03:05 +0530

    added more props and styling

--- apps/web/components/QtySelector.tsx
+++ apps/web/components/QtySelector.tsx
@@ -1,13 +1,25 @@
 "use client";
 import { Button } from "@repo/ui";
-import { useState } from "react";
+import { Minus, Plus } from "lucide-react";
+import { HTMLAttributes, useState } from "react";
+
+type QtySelectorProps = {
+  initialCount?: number;
+  onChange?: (count: number) => void;
+  buttonHeight?: number; //Tailwind CSS height value
+};
 
 export default function QtySelector({
   initialCount,
-}: {
-  initialCount?: number;
-}) {
-  const [count, setCount] = useState(initialCount || 1);
+  onChange,
+  buttonHeight,
+}: QtySelectorProps) {
+  const [count, setQty] = useState(initialCount || 1);
+
+  const setCount = (qty: number) => {
+    setQty(qty);
+    onChange?.(qty);
+  };
 
   return (
     <div className="text-xl flex gap-1 items-center">
@@ -23,19 +35,19 @@ export default function QtySelector({
       <Button
         onClick={() => setCount(Math.max(1, count - 1))}
         type="button"
-        className="border h-8 aspect-square text-3xl"
+        className={`border h-${buttonHeight || 8} aspect-square p-1`}
         title="Decrease quantity"
       >
-        -
+        <Minus />
       </Button>
-      <span className="w-6">{count}</span>
+      <span className="w-6 text-center">{count}</span>
       <Button
         onClick={() => setCount(Math.min(30, count + 1))}
         type="button"
-        className="border h-8 aspect-square text-3xl"
+        className={`border h-${buttonHeight || 8} aspect-square p-1`}
         title="Increase quantity"
       >
-        +
+        <Plus />
       </Button>
     </div>
   );
