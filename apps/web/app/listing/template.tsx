246c90dcc6159d04a68403d1975f6b0b7e83e1d4
Harsh Dagar <amandagar000@gmail.com>
2024-04-16 00:13:05 +0530

    removed❌: filters panel (no support from api)

--- apps/web/app/listing/template.tsx
+++ apps/web/app/listing/template.tsx
@@ -8,7 +8,7 @@ type ListingProp = {
 export default function ListingTemplate({ children, ...rest }: ListingProp) {
   return (
     <div className="grid grid-cols-4 px-common-x gap-10">
-      <div className="border max-64">Filters</div>
+      {/* <div className="border max-64">Filters</div> */}
       {children}
     </div>
   );
