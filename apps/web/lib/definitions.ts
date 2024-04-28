b922af3fc2e546d15248bde742e9385734e1a128
Harsh Dagar <amandagar000@gmail.com>
2024-04-28 22:28:49 +0530

    add login/signup verification schema with zod     * added action return types

---
+++ apps/web/lib/definitions.ts
@@ -0,0 +1,67 @@
+import { z } from "zod";
+
+export const LoginSchema = z.object({
+  email: z.string().email("Please provide valid credentials").trim(),
+  password: z.string().min(8, "Please provide valid credentials").trim(),
+});
+export const SignupSchema = z
+  .object({
+    firstName: z.string().min(1, "First name is required").trim(),
+    lastName: z.string().trim().optional().default(""),
+    email: z.string().email({ message: "Please provide a valid email" }).trim(),
+    dob: z
+      .date()
+      .max(
+        new Date(new Date().setFullYear(new Date().getFullYear() - 16)),
+        "Minimum age should be more than 16"
+      )
+      .optional()
+      .default(new Date("1 Jan 2000")),
+    gender: z.enum(["male", "female", "other"]).optional().default("other"),
+    password: z
+      .string()
+      .min(8, "Password must be at least 8 characters")
+      .regex(
+        /[^a-zA-Z0-9]+/,
+        "Password must contain at least one special character"
+      )
+      .regex(/[A-Z]+/, "Password must contain at least one uppercase letter")
+      .regex(/[a-z]+/, "Password must contain at least one lowercase letter")
+      .regex(/[0-9]+/, "Password must contain at least one digit")
+      .trim(),
+    passwordConfirm: z.string().trim(),
+  })
+  .superRefine(({ password, passwordConfirm }, ctx) => {
+    if (password !== passwordConfirm) {
+      ctx.addIssue({
+        message: "Passwords do not match",
+        path: ["passwordConfirm"],
+        code: "custom",
+      });
+    }
+  });
+
+export type LoginFormErrorState =
+  | {
+      error?: {
+        email?: string[];
+        password?: string[];
+      };
+    }
+  | {
+      message?: string[];
+    }
+  | undefined;
+export type SignupFormErrorState =
+  | {
+      error?: {
+        firstName?: string[];
+        email?: string[];
+        password?: string[];
+        passwordConfirm?: string[];
+        dob?: string[];
+        gender?: string[];
+      };
+      message?: string[];
+    }
+  | undefined;
