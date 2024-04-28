bfbe2e4ae47ec6150729f92a512ecc590b87f6c2
Harsh Dagar <amandagar000@gmail.com>
2024-04-28 22:26:50 +0530

    create token creator/verifier with jose

---
+++ apps/web/lib/jwt.ts
@@ -0,0 +1,26 @@
+import { JWTPayload, jwtVerify, SignJWT } from "jose";
+
+const secretKey = process.env.SECRET_KEY;
+const encodedKey = new TextEncoder().encode(secretKey);
+
+const createToken = async (payload: JWTPayload) => {
+  return await new SignJWT(payload)
+    .setProtectedHeader({ alg: "HS256" })
+    .setIssuedAt()
+    .setExpirationTime(process.env.SESSION_EXPIRY || "30m")
+    .sign(encodedKey);
+};
+
+const verifyToken = async (token: string) => {
+  try {
+    const { payload } = await jwtVerify(token, encodedKey, {
+      algorithms: ["HS256"],
+    });
+    return payload;
+  } catch (e) {
+    console.log("TokenVerificationFailed", e);
+    return null;
+  }
+};
+
+export { createToken, verifyToken };
