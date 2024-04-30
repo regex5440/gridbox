94d635737f1498abd492030001599dda340cfd7d
Harsh Dagar <amandagar000@gmail.com>
2024-05-01 02:40:11 +0530

    rename & expose hash compare functions

--- apps/web/lib/bcrypt.ts
+++ apps/web/lib/bcrypt.ts
@@ -1,8 +1,12 @@
-import { hash as bcryptHsh, genSaltSync } from "bcrypt";
+import { hash as bcryptHsh, compare, genSaltSync } from "bcrypt";
 
 const providedSaltRounds = Number(process.env.PASSWORD_HASH_SALT) || 10;
 const salt = genSaltSync(providedSaltRounds);
 
 export const hash = async (password: string) => {
   return bcryptHsh(password, salt);
+};
+
+export const compareHash = async (data: string, hashedData: string) => {
+  return compare(data, hashedData);
 };
