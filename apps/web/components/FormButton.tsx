bf57a8e973a5b0e90f33e5a134b52a310650cd6c
Harsh Dagar <amandagar000@gmail.com>
2024-05-09 00:29:49 +0530

    add loader for pending form status

--- apps/web/components/FormButton.tsx
+++ apps/web/components/FormButton.tsx
@@ -1,11 +1,21 @@
 "use client";
 import { Button } from "@repo/ui";
+import { LoaderCircle } from "lucide-react";
 import { HTMLAttributes, forwardRef } from "react";
 import { useFormStatus } from "react-dom";
 
 export default forwardRef<HTMLButtonElement, HTMLAttributes<HTMLButtonElement>>(
-  function FormButton({ ...props }, ref) {
+  function FormButton({ children, ...props }, ref) {
     const { pending } = useFormStatus();
-    return <Button {...props} data-pending={pending} ref={ref} />;
+    return (
+      <Button
+        {...props}
+        data-pending={pending}
+        ref={ref}
+        children={
+          pending ? <LoaderCircle className="animate-spin" /> : children
+        }
+      />
+    );
   }
 );
