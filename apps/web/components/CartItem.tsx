5251196a8551f9da59c80d05ec9cf1c01eb9484d
Harsh Dagar <amandagar000@gmail.com>
2024-05-11 01:14:41 +0530

    work with ssr data + data fetch option for client

--- apps/web/components/CartItem.tsx
+++ apps/web/components/CartItem.tsx
@@ -10,14 +10,23 @@ import { useState } from "react";
 import { useFetchProduct } from "@hooks/index";
 
 type CartItemProps = {
-  productId: string;
   initialQty?: number;
   onRemove?: (productId: string) => void;
   onQtyChange?: (qty: number, productId: string) => void;
   className?: string;
-};
+  productId?: string;
+  productObj?: Product;
+} & (
+  | {
+      productId: string;
+    }
+  | {
+      productObj: Product;
+    }
+);
 
 export default function CartItem({
+  productObj,
   productId,
   initialQty = 1,
   onQtyChange,
@@ -25,51 +34,102 @@ export default function CartItem({
   className = "",
 }: CartItemProps) {
   const [quantity, setQuantity] = useState(initialQty);
-  const { data: product, isLoading, error } = useFetchProduct(productId);
 
-  const handleQtyChange = (count: number) => {
+  const handleQtyChange = (count: number, id: string) => {
     setQuantity(count);
-    onQtyChange?.(count, productId);
+    onQtyChange?.(count, id);
     //TODO: update cart item quantity (debounced?)
   };
+  //TODO: style the qty selector based on minicart/cart page/product page
 
   return (
     <div className={"flex gap-4 border-b pb-2 lg:max-w-lg w-full " + className}>
+      {productObj ? (
+        <CartItemWithProduct
+          product={productObj}
+          quantity={quantity}
+          handleQtyChange={handleQtyChange}
+          onRemove={onRemove}
+        />
+      ) : productId ? (
+        <CartItemProductFetch
+          productId={productId}
+          quantity={quantity}
+          handleQtyChange={handleQtyChange}
+          onRemove={onRemove}
+        />
+      ) : null}
+    </div>
+  );
+}
+
+type CommonProps = {
+  quantity: number;
+  handleQtyChange: (count: number, id: string) => void;
+  onRemove?: (productId: string) => void;
+};
+
+function CartItemWithProduct({
+  product,
+  quantity,
+  handleQtyChange,
+  onRemove,
+}: { product: Product } & CommonProps) {
+  return (
+    <>
+      <Link href={`${SiteMap.PDP.path}/${product.id}`}>
+        <div className="relative w-24 h-24">
+          <Image src={product.thumbnail} alt={product.title} fill />
+          {/* <div className="absolute bottom-0.5 right-0.5 rounded-full aspect-square w-fit p-1 flex justify-center items-center backdrop-invert">
+    <span className="text-sm font-semibold"></span>
+  </div> */}
+        </div>
+      </Link>
+      <div className="flex flex-col justify-between w-full">
+        <Link href={`${SiteMap.PDP.path}/${product.id}`}>
+          <h3 className="text-xl line-clamp-1">{product.title}</h3>
+        </Link>
+        <p>${product.price}</p>
+        <div className="flex justify-between items-center">
+          <QtySelector
+            count={quantity}
+            onChange={(c) => handleQtyChange(c, String(product.id))}
+            triggerClassName="group-[.minicart]:w-10 w-12 h-5 px-1 py-0.5"
+            optionClassName="py-0 px-0 "
+          />
+          <Button
+            onClick={onRemove?.bind(null, String(product.id))}
+            title="Remove"
+            className="text-secondary w-7 h-7 p-1 bg-transparent"
+          >
+            <Trash2 />
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+function CartItemProductFetch({
+  productId,
+  quantity,
+  handleQtyChange,
+  onRemove,
+}: { productId: string } & CommonProps) {
+  const { data: product, isLoading, error } = useFetchProduct(productId);
+
+  return (
+    <>
       {isLoading && <p>Loading...</p>}
       {error && <p>Cannot load product</p>}
       {product && (
-        <>
-          <Link href={`${SiteMap.PDP.path}/${product.id}`}>
-            <div className="relative w-24 h-24">
-              <Image src={product.thumbnail} alt={product.title} fill />
-              {/* <div className="absolute bottom-0.5 right-0.5 rounded-full aspect-square w-fit p-1 flex justify-center items-center backdrop-invert">
-            <span className="text-sm font-semibold"></span>
-          </div> */}
-            </div>
-          </Link>
-          <div className="flex flex-col justify-between w-full">
-            <Link href={`${SiteMap.PDP.path}/${product.id}`}>
-              <h3 className="text-xl line-clamp-1">{product.title}</h3>
-            </Link>
-            <p>${product.price}</p>
-            <div className="flex justify-between items-center">
-              <QtySelector
-                count={quantity}
-                onChange={handleQtyChange}
-                buttonClassName="group-[.minicart]:w-6 group-[.minicart]:h-6 p-1 text-regular cursor-pointer bg-transparent border-2 border-primary"
-                countClassName="text-base w-7 text-center text-primary"
-              />
-              <Button
-                onClick={onRemove?.bind(null, String(product.id))}
-                title="Remove"
-                className="group-[.minicart]:text-secondary group-[.minicart]:w-7 group-[.minicart]:h-7 group-[.minicart]:p-1 group-[.minicart]:bg-transparent"
-              >
-                <Trash2 />
-              </Button>
-            </div>
-          </div>
-        </>
+        <CartItemWithProduct
+          product={product}
+          quantity={quantity}
+          handleQtyChange={handleQtyChange}
+          onRemove={onRemove}
+        />
       )}
-    </div>
+    </>
   );
 }
