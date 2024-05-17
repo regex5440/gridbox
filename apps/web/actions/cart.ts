288b7d67f53c738b2833b005e85c5534dc9f5fae
Harsh Dagar <amandagar000@gmail.com>
2024-05-18 01:22:34 +0530

    create common function for GET cart breakup

--- apps/web/actions/cart.ts
+++ apps/web/actions/cart.ts
@@ -4,6 +4,7 @@ import { authenticateUser } from "./auth";
 import { ProductPurchaseFormState } from "@types";
 import { CartProductAddSchema } from "@lib/definitions";
 import { removeItemFromCart, updateItemQuantity } from "controllers/cart";
+import { CartItem } from "@repo/ui/types";
 
 export async function buyNowAction(
   state: ProductPurchaseFormState,
@@ -93,4 +94,45 @@ export async function removeCartItem(productId: string) {
       },
     },
   };
+}
+
+export async function getCartBreakup(cartItems: CartItem[] | null) {
+  const cartTotal = {
+    subTotal: 0,
+    maxDiscountPercentage: 0,
+    discountedPrice: 0,
+    tax: 0,
+    payable: 0,
+  };
+
+  if (!cartItems) {
+    return cartTotal;
+  }
+  const cartProducts = await Promise.all(
+    cartItems.map(({ productId }) =>
+      fetch(
+        `${process.env.productAPI}/products/${productId}?select=discountPercentage,price`
+      ).then((res) => res.json())
+    )
+  );
+
+  const subTotal = cartProducts.reduce(
+    (acc: number, product, index) =>
+      acc + product.price * cartItems[index].quantity,
+    0
+  );
+  const maxDiscountPercentage = cartProducts.reduce(
+    (acc: number, product) =>
+      acc > product.discountPercentage ? acc : product.discountPercentage,
+    0
+  );
+  const discountedPrice = subTotal * (maxDiscountPercentage / 100);
+  const tax = discountedPrice * 0.1; // 10%
+
+  cartTotal.subTotal = Number(subTotal.toFixed(2));
+  cartTotal.maxDiscountPercentage = Number(maxDiscountPercentage.toFixed(2));
+  cartTotal.discountedPrice = Number(discountedPrice.toFixed(2));
+  cartTotal.tax = Number(tax.toFixed(2));
+  cartTotal.payable = Number((subTotal - discountedPrice + tax).toFixed(2));
+  return cartTotal;
 }
--- apps/web/app/api/(routes)/(protected)/cart/total/route.ts
+++ apps/web/app/api/(routes)/(protected)/cart/total/route.ts
@@ -1,6 +1,7 @@
 import { authenticateUser } from "actions/auth";
 import { getCartItems } from "controllers/cart";
 import { NextRoute } from "@types";
+import { getCartBreakup } from "@actions/cart";
 
 export const GET: NextRoute = async (request, { params }) => {
   try {
@@ -12,44 +13,8 @@ export const GET: NextRoute = async (request, { params }) => {
     }
     const cartItems = await getCartItems({ userId: authenticUser.data.id });
 
-    const cartTotal = {
-      subTotal: 0,
-      maxDiscountPercentage: 0,
-      discountedPrice: 0,
-      tax: 0,
-      payable: 0,
-    };
-
-    if (!cartItems.data) {
-      return Response.json({ success: { data: cartTotal } });
-    }
-    const cartProducts = await Promise.all(
-      cartItems.data.map(({ productId }) =>
-        fetch(`${process.env.productAPI}/products/${productId}`).then((res) =>
-          res.json()
-        )
-      )
-    );
-
-    const subTotal = cartProducts.reduce(
-      (acc: number, product, index) =>
-        acc + product.price * cartItems.data[index].quantity,
-      0
-    );
-    const maxDiscountPercentage = cartProducts.reduce(
-      (acc: number, product) =>
-        acc > product.discountPercentage ? acc : product.discountPercentage,
-      0
-    );
-    const discountedPrice = subTotal * (maxDiscountPercentage / 100);
-    const tax = discountedPrice * 0.1; // 10%
-
-    cartTotal.subTotal = Number(subTotal.toFixed(2));
-    cartTotal.maxDiscountPercentage = Number(maxDiscountPercentage.toFixed(2));
-    cartTotal.discountedPrice = Number(discountedPrice.toFixed(2));
-    cartTotal.tax = Number(tax.toFixed(2));
-    cartTotal.payable = Number((subTotal - discountedPrice + tax).toFixed(2));
-    return Response.json({ success: { data: cartTotal } });
+    const data = await getCartBreakup(cartItems.data);
+    return Response.json({ success: { data } });
   } catch (error) {
     return Response.json(
       { error: { message: "Failed to get cart total" } },
