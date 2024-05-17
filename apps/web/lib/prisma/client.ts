1956452cc30d61165792dd16a95d96edf399bad5
Harsh Dagar <amandagar000@gmail.com>
2024-05-18 01:25:03 +0530

    prevent multiple clients during development

--- apps/web/lib/prisma/client.ts
+++ apps/web/lib/prisma/client.ts
@@ -1,12 +1,23 @@
 import { PrismaClient } from "prisma/prisma-client";
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
 
-const prisma = new PrismaClient();
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
 
-try {
-  prisma.$connect();
-} catch (e) {
-  console.log(e);
-  prisma.$disconnect();
-}
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+// try {
+//   prisma.$connect();
+// } catch (e) {
+//   console.log(e);
+//   prisma.$disconnect();
+// }
 
 export default prisma;
+
+if (process.env.NODE_ENV === "development") {
+  globalThis.prismaGlobal = prisma;
+}
