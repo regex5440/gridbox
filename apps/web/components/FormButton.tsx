de0e3bc9d83ac819bba0a3adbe2b8dbf66cc9b37
Harsh Dagar <amandagar000@gmail.com>
2024-06-01 16:33:56 +0530

    modify attribute types

--- apps/web/components/FormButton.tsx
+++ apps/web/components/FormButton.tsx
@@ -1,21 +1,20 @@
 "use client";
 import { Button } from "@repo/ui";
 import { LoaderCircle } from "lucide-react";
-import { HTMLAttributes, forwardRef } from "react";
+import { ButtonHTMLAttributes, forwardRef } from "react";
 import { useFormStatus } from "react-dom";
 
-export default forwardRef<HTMLButtonElement, HTMLAttributes<HTMLButtonElement>>(
-  function FormButton({ children, ...props }, ref) {
-    const { pending } = useFormStatus();
-    return (
-      <Button
-        {...props}
-        data-pending={pending}
-        ref={ref}
-        children={
-          pending ? <LoaderCircle className="animate-spin" /> : children
-        }
-      />
-    );
-  }
-);
+export default forwardRef<
+  HTMLButtonElement,
+  ButtonHTMLAttributes<HTMLButtonElement>
+>(function FormButton({ children, ...props }, ref) {
+  const { pending } = useFormStatus();
+  return (
+    <Button
+      {...props}
+      data-pending={pending}
+      ref={ref}
+      children={pending ? <LoaderCircle className="animate-spin" /> : children}
+    />
+  );
+});
