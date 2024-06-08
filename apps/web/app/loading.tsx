12ec9da456960155c21dcd332c571eb19e6749cc
Harsh Dagar <amandagar000@gmail.com>
2024-06-09 03:13:52 +0530

    loader without empty screen

--- apps/web/app/loading.tsx
+++ apps/web/app/loading.tsx
@@ -2,11 +2,16 @@ import { Loader2 } from "lucide-react";
 
 export default function Loading() {
   return (
-    <div className="fixed inset-0 z-50 bg-transparent backdrop-blur-sm flex justify-center items-center flex-col">
-      <div className="animate-spin">
-        <Loader2 className="stroke-primary" size="50" />
+    <div>
+      <div className="grid place-content-center h-[60vh] w-full">
+        <p className="text-xl">Loading...</p>
       </div>
-      GridBox getting ready🚀
+      <div className="fixed inset-0 z-[100] bg-transparent backdrop-blur-sm flex justify-center items-center flex-col">
+        <div className="animate-spin">
+          <Loader2 className="stroke-primary" size="50" />
+        </div>
+        GridBox getting ready🚀
+      </div>
     </div>
   );
 }
