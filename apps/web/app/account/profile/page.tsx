aaf5f470ecbaf13238dea05f0735ca0374766b7e
Harsh Dagar <amandagar000@gmail.com>
2024-06-22 18:13:25 +0530

    temporary profile detail page

---
+++ apps/web/app/account/profile/page.tsx
@@ -0,0 +1,9 @@
+export default function Profile() {
+  //TODO: Implement update profile
+  return (
+    <div>
+      <h1 className="text-2xl font-semibold">Update Profile</h1>
+      <p>Upcoming with new release</p>
+    </div>
+  );
+}
