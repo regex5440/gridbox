d9ca19e0e0feafdb0748bf73fe37e4b0bb6bbc63
Harsh Dagar <amandagar000@gmail.com>
2024-06-24 15:54:42 +0530

    update error page

---
+++ apps/web/app/global-error.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function ErrorPage() {
+  return (
+    <div className="w-full min-h-[40vh] grid place-content-center">
+      <h1>Oops! This page does not exist</h1>
+    </div>
+  );
+}
