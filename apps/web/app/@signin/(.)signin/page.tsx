e61ba41ab6c33359e8e4963af59535b1c575de3d
Harsh Dagar <amandagar000@gmail.com>
2024-06-24 16:43:38 +0530

    different alignment in mobile for signin modal

--- apps/web/app/@signin/(.)signin/page.tsx
+++ apps/web/app/@signin/(.)signin/page.tsx
@@ -14,7 +14,7 @@ export default function LoginSignupModal() {
       className="fixed left-0 top-0 z-[51] w-[100vw] h-[100vh] bg-overlay backdrop-blur-sm"
       onClick={routeBack}
     ></div>,
-    <div className="fixed-centered z-[52] w-full md:max-w-md max-md:bottom-0 max-md:rounded-l-2xl max-md:rounded-r-2xl group/signin">
+    <div className="fixed sm:top-1/2 sm:left-1/2 sm:-translate-x-1/2 sm:-translate-y-1/2 max-sm:animate-slide-bottom-up max-sm:bottom-0 max-sm:left-0 max-sm:rounded-b-none z-[52] w-full md:max-w-md max-md:rounded-l-2xl max-md:rounded-r-2xl group/signin">
       <div className="relative">
         <div
           className="absolute right-2 top-2 p-2 cursor-pointer"
