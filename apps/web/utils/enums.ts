f7cb8c4c849b81f579abca22edc2c001f38506df
Harsh Dagar <amandagar000@gmail.com>
2024-06-26 22:01:20 +0530

    remove categories enum

--- apps/web/utils/enums.ts
+++ apps/web/utils/enums.ts
@@ -1,33 +1,5 @@
 import { Order } from "@repo/ui/types";
 
-export const Category = {
-  ALL: "all",
-  SMARTPHONE: "smartphones",
-  LAPTOPS: "laptops",
-  SKINCARE: "skincare",
-  FURNITURE: "furniture",
-  "HOME DECORATION": "home-decoration",
-  TOPS: "tops",
-  MEN: {
-    SHIRTS: "mens-shirts",
-    SHOES: "mens-shoes",
-    WATCHES: "mens-watches",
-  },
-  WOMEN: {
-    DRESSES: "womens-dresses",
-    SHOES: "womens-shoes",
-    BAGS: "womens-bags",
-    JEWELRY: "womens-jewellery",
-    WATCHES: "womens-watches",
-  },
-  LIGHTNING: "lighting",
-  AUTOMOTIVE: "automotive",
-  SUNGLASSES: "sunglasses",
-  GROCERIES: "groceries",
-  FRAGRANCES: "fragrances",
-  MOTORCYCLE: "motorcycle",
-};
-
 export const ORDER_STATUS: Record<
   Pick<Order, "status">["status"],
   { label: string; color: string }
