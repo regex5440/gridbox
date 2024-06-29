e30d17b343e4bb190c27ffd158aa8bab153a50f4
Harsh Dagar <amandagar000@gmail.com>
2024-06-29 16:17:34 +0530

    update error message

--- apps/web/app/global-error.tsx
+++ apps/web/app/global-error.tsx
@@ -3,7 +3,7 @@
 export default function ErrorPage() {
   return (
     <div className="w-full min-h-[40vh] grid place-content-center">
-      <h1>Oops! This page does not exist</h1>
+      <h1>Something went wrong! Try to refresh the site!</h1>
     </div>
   );
 }
