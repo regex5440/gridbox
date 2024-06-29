4700c0ef34d1f66ea7455573f0e2abac00ea88bb
Harsh Dagar <amandagar000@gmail.com>
2024-06-29 16:54:13 +0530

    replace order route with page redirect

---
+++ apps/web/app/account/orders/page.tsx
@@ -0,0 +1,6 @@
+import SiteMap from "@utils/sitemap";
+import { RedirectType, redirect } from "next/navigation";
+
+export default function RedirectOrders() {
+  redirect(`${SiteMap.Account.Orders.path}/1?ps=10`, RedirectType.replace);
+}
--- apps/web/app/account/orders/route.ts
+++
@@ -1,6 +0,0 @@
-import { RedirectType, redirect } from "next/navigation";
-import type { NextRequest } from "next/server";
-
-export function GET(request: NextRequest) {
-  return redirect(`${request.nextUrl.pathname}/1?ps=10`, RedirectType.replace);
-}
