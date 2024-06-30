be62ffc6699fd24eaeddbc8e375cf8b25ba786de
Harsh Dagar <amandagar000@gmail.com>
2024-06-30 23:30:48 +0530

    lint: ignore no await, async function

--- packages/config-eslint/next.js
+++ packages/config-eslint/next.js
@@ -72,7 +72,8 @@ module.exports = {
     "@typescript-eslint/no-unsafe-member-access": "off",
     "@typescript-eslint/no-unsafe-argument": "off",
     "@typescript-eslint/no-unsafe-assignment": "off",
-
+    "@typescript-eslint/no-misused-promises": "off",
+    "@typescript-eslint/require-await": "off",
     "tsdoc/syntax": "off",
     "import/named": "off",
     "no-nested-ternary": "off",
