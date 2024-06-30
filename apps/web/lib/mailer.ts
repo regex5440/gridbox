10440c1650d253da1c9d58186383c48bdd49db57
Harsh Dagar <amandagar000@gmail.com>
2024-06-30 23:03:53 +0530

    update mailer to use mailer service

--- apps/web/lib/mailer.ts
+++ apps/web/lib/mailer.ts
@@ -1,40 +1,27 @@
 "use server";
-import { createTransport } from "nodemailer";
 
-const mailTransporter = createTransport({
-  host: process.env.EMAIL_SERVER,
-  port: Number(process.env.EMAIL_PORT) || 465,
-  secure: true,
-  auth: {
-    user: process.env.EMAIL_USERNAME,
-    pass: process.env.EMAIL_PASSWORD,
-  },
-});
-
-/**
- *
- * @param param.to Email address of recipient
- * @param param.subject Subject line of the Email
- * @param param.html HTML body of the email
- * @param param.sender Sender info @default sender Chit-Chat <${process.env.EMAIL_USERNAME}>
- * @returns
- */
 const sendEmail = async ({
   to,
   subject,
-  sender = `${process.env.EMAIL_FROM || "info"} <${process.env.EMAIL_USERNAME}>`,
   html,
 }: {
   to: string;
   subject: string;
-  sender?: string;
   html: string;
 }) => {
-  return mailTransporter.sendMail({
-    from: sender,
-    to,
-    subject,
-    html,
+  return fetch(process.env.EMAIL_SERVICE as string, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.EMAIL_SERVICE_TOKEN}`,
+    },
+    body: JSON.stringify({
+      fromService: "GridBox",
+      toEmail: to,
+      subject,
+      html,
+      allowReply: true,
+    }),
   });
 };
 
